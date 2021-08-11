d86a49fd7040b93d80add8875441b61d33600cd5
quanshubli <309063267@qq.com>
2021-08-11 23:33:25 +0800

    promise

--- promise/index.js
+++ promise/index.js
@@ -1,11 +1,16 @@
 /**
  * 实现简易的Promise
- * 定时器问题待解决
+ * 遗留问题：
+ * 1.前一个then调用onRejected时，后面的then仍会调用onFulfilled
+ * 2.js原生Promise产生的then是微任务
  */
 class MyPromise {
   constructor(callback) {
     this.state = 'pending'
     this.result = null
+
+    this.fulfilledCallbacks = []
+    this.rejectedCallbacks = []
 
     this._resolve = this._resolve.bind(this)
     this._reject = this._reject.bind(this)
@@ -21,6 +26,8 @@ class MyPromise {
     if (this.state === 'pending') {
       this.state = 'fulfilled'
       this.result = value
+
+      this.fulfilledCallbacks.reduce((result, cb) => cb(result), this.result)
     }
   }
 
@@ -28,23 +35,42 @@ class MyPromise {
     if (this.state === 'pending') {
       this.state = 'rejected'
       this.result = value
+
+      this.rejectedCallbacks.reduce((result, cb) => cb(result), this.result)
     }
   }
 
   then(onFulfilled, onRejected) {
-    if (this.state === 'fulfilled') {
-      onFulfilled(this.result)
-    } else if (this.state === 'rejected') {
-      onRejected(this.result)
-    }
+    // 用const无法在thenPromise内部使用自身变量
+    let thenPromise
+
+    thenPromise = new MyPromise((resolve, reject) => {
+      const handle = cb => {
+        try {
+          const result = cb(this.result)
+
+          if (result === thenPromise) {
+            throw new Error('不能返回自身')
+          } else if (result instanceof MyPromise) {
+            result.then(resolve, reject)
+          } else {
+            resolve(result)
+          }
+        } catch (err) {
+          reject(err)
+        }
+      }
+
+      if (this.state === 'fulfilled') {
+        handle(onFulfilled)
+      } else if (this.state === 'rejected') {
+        handle(onRejected)
+      } else {
+        this.fulfilledCallbacks.push(handle.bind(this, onFulfilled))
+        this.rejectedCallbacks.push(handle.bind(this, onRejected))
+      }
+    })
+
+    return thenPromise
   }
 }
-
-const promise = new MyPromise((resolve, reject) => {
-  // throw new Error('throw error')
-  // resolve('成功')
-  // reject('失败')
-  setTimeout(() => {
-    resolve('成功')
-  }, 1000)
-}).then(console.log, console.log)
