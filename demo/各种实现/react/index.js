479801ffe4d5e8c47e793b2fc90f0d7b397f517b
quanshubli <309063267@qq.com>
2022-01-28 15:01:34 +0800

    update: react实现

--- demo/各种实现/react/index.js
+++ demo/各种实现/react/index.js
@@ -25,40 +25,122 @@ function createTextElement(text) {
   }
 }
 
-// 递归的缺点:
-// 递归无法中断，对于比较结构深的节点树，递归需要花费大量时间，长期占用浏览器主线程，造成页面的卡顿。
-
-// 一个节点对应一个fiber，一个fiber就是一个任务单元
-
-// render创建root fiber，将其作为nextUnitOfWork，剩下的交给performUnitOfWork
-function render(element, container) {
-  const dom = element.type === 'TEXT_ELEMENT'
+function createDom(fiber) {
+  const dom = fiber.type === 'TEXT_ELEMENT'
     ? document.createTextNode('')
-    : document.createElement(element.type)
+    : document.createElement(fiber.type)
 
-  const { children, ...restProps } = element.props
+  const { children, ...restProps } = fiber.props
 
   Object.keys(restProps).forEach(key => {
     dom[key] = restProps[key]
   })
 
-  children.forEach(item => {
-    render(item, dom)
-  })
+  return dom
+}
 
-  container.appendChild(dom)
+const isEvent = key => key.startsWith('on')
+const isProperty = key => key !== 'children' && !isEvent(key)
+const isNew = (prev, next) => key => prev[key] !== next[key]
+const isGone = (prev, next) => key => !(key in next)
+function updateDom(dom, prevProps, nextProps) {
+  // 去掉老的或者改变的事件监听
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter(key => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+    .forEach(name => {
+      const eventType = name.toLowerCase().substring(2)
+      dom.removeEventListener(eventType, prevProps[name])
+    })
+
+  // 设置新的事件监听
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      const eventType = name.toLowerCase().substring(2)
+      dom.addEventListener(eventType, nextProps[name])
+    })
+
+  // 去掉消失的props
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = ''
+    })
+
+  // 设置新的或者改变的props
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = nextProps[name]
+    })
+}
+
+function commitRoot() {
+  deletions.forEach(commitWork)
+  commitWork(wipRoot.child)
+  currentRoot = wipRoot
+  wipRoot = null
+}
+
+function commitWork(fiber) {
+  if (!fiber) {
+    return
+  }
+
+  const domParent = fiber.parent.dom
+
+  if (fiber.effectTag === 'PLACEMENT' && fiber.dom != null) {
+    domParent.appendChild(fiber.dom)
+  } else if (fiber.effectTag === 'DELETION') {
+    domParent.removeChild(fiber.dom)
+  } else if (fiber.effectTag === 'UPDATE') {
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props)
+  }
+
+  commitWork(fiber.child)
+  commitWork(fiber.sibling)
+}
+
+// 递归的缺点:
+// 递归无法中断，对于结构比较复杂的节点树，递归需要花费大量时间，长期占用浏览器主线程，造成页面的卡顿。
+
+// 一个节点对应一个fiber，一个fiber就是一个任务单元
+// render创建root fiber，将其作为nextUnitOfWork，剩下的交给performUnitOfWork
+function render(element, container) {
+  wipRoot = {
+    dom: container,
+    props: {
+      children: [element]
+    },
+    alternate: currentRoot  // alternate指向上一次提交到页面的fiber
+  }
+
+  deletions = []
+  nextUnitOfWork = wipRoot
 }
 
 let nextUnitOfWork = null
+let currentRoot = null  // 当前页面中的root fiber
+let wipRoot = null
+let deletions = null
 
 function workLoop(deadline) {
   let shouldYield = false
 
   while (nextUnitOfWork && !shouldYield) {
-    // performUnitOfWork做优化，并返回下一个任务单元
+    // performUnitOfWork处理任务，并返回下一个任务单元
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
     // deadline.timeRemaining()返回一个时间，表示当前闲置周期的预估剩余毫秒数
     shouldYield = deadline.timeRemaining() < 1
+  }
+
+  // 所有任务完成时，将整个fiber树插入到dom中
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot()
   }
 
   requestIdleCallback(workLoop)
@@ -69,11 +151,95 @@ requestIdleCallback(workLoop)
 /**
  * 做3件事：
  * 1.将节点加入DOM
- * 2.创建该节点下的字节点的fiber
+ * 2.创建该节点下的子节点的fiber
  * 3.选择下一个任务单元
  */
-function performUnitOfWork(nextUnitOfWork) {
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
 
+  // 因为任务是在浏览器空闲时处理的，所以多个任务之间会被中断。
+  // 那么在每个任务单元中插入dom，会在页面中显示不完整的UI。
+  // 所以插入dom的操作不能在这里进行。
+  // 将节点加入DOM
+  // if (fiber.parent) {
+  //   fiber.parent.dom.appendChild(fiber.dom)
+  // }
+
+  // 创建该节点下的子节点的fiber
+  const elements = fiber.props.children
+  reconcileChildren(fiber, elements)
+
+  // 返回下一个任务单元
+  if (fiber.child) {
+    return fiber.child
+  }
+
+  let nextFiber = fiber
+
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
+}
+
+function reconcileChildren(wipFiber, elements) {
+  let index = 0
+  let oldFiber = wipFiber.alternate && wipFiber.alternate.child
+  let prevSibling = null
+
+  // elements是想要渲染的fiber，oldFiber是上一次渲染的fiber
+
+  while (index < elements.length || oldFiber != null) {
+    const element = elements[index]
+    let newFiber = null
+
+    // 比较前后两次的fiber
+    // 1.type相同，仅改变节点的props
+    // 2.type不同，删除前一次的节点，创建新的节点
+    const sameType = oldFiber && element && element.type === oldFiber.type
+
+    if (sameType) {
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: 'UPDATE'
+      }
+    }
+    if (element && !sameType) {
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: 'PLACEMENT'
+      }
+    }
+    if (oldFiber && !sameType) {
+      oldFiber.effectTag = 'DELETION'
+      deletions.push(oldFiber)
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling
+    }
+
+    if (index === 0) {
+      wipFiber.child = newFiber
+    } else if (element) {
+      prevSibling.sibling = newFiber
+    }
+    
+    prevSibling = newFiber
+    index++
+  }
 }
 
 const Didact = {
