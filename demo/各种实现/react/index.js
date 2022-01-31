f7fcd174aa8efd7d72ce1ab22f82038398d1eaac
quanshubli <309063267@qq.com>
2022-01-31 15:38:13 +0800

    update: build your own react

--- demo/各种实现/react/index.js
+++ demo/各种实现/react/index.js
@@ -30,11 +30,7 @@ function createDom(fiber) {
     ? document.createTextNode('')
     : document.createElement(fiber.type)
 
-  const { children, ...restProps } = fiber.props
-
-  Object.keys(restProps).forEach(key => {
-    dom[key] = restProps[key]
-  })
+  updateDom(dom, {}, fiber.props)
 
   return dom
 }
@@ -91,18 +87,32 @@ function commitWork(fiber) {
     return
   }
 
-  const domParent = fiber.parent.dom
+  // 向上查找，直至找到有dom的fiber
+  let domParentFiber = fiber.parent
+  while (!domParentFiber.dom) {
+    domParentFiber = domParentFiber.parent
+  }
+  const domParent = domParentFiber.dom
 
   if (fiber.effectTag === 'PLACEMENT' && fiber.dom != null) {
     domParent.appendChild(fiber.dom)
   } else if (fiber.effectTag === 'DELETION') {
-    domParent.removeChild(fiber.dom)
-  } else if (fiber.effectTag === 'UPDATE') {
+    // 删除时也要查找存在dom的fiber
+    commitDeletion(fiber, domParent)
+  } else if (fiber.effectTag === 'UPDATE' && fiber.dom != null) {
     updateDom(fiber.dom, fiber.alternate.props, fiber.props)
   }
 
   commitWork(fiber.child)
   commitWork(fiber.sibling)
+}
+
+function commitDeletion(fiber, domParent) {
+  if (fiber.dom) {
+    domParent.removeChild(fiber.dom)
+  } else {
+    commitDeletion(fiber.child, domParent)
+  }
 }
 
 // 递归的缺点:
@@ -150,13 +160,17 @@ requestIdleCallback(workLoop)
 
 /**
  * 做3件事：
- * 1.将节点加入DOM
+ * 1.将节点加入DOM ❌
  * 2.创建该节点下的子节点的fiber
  * 3.选择下一个任务单元
  */
 function performUnitOfWork(fiber) {
-  if (!fiber.dom) {
-    fiber.dom = createDom(fiber)
+  const isFunctionComponent = fiber.type instanceof Function
+
+  if (isFunctionComponent) {
+    updateFunctionComponent(fiber)
+  } else {
+    updateHostComponent(fiber)
   }
 
   // 因为任务是在浏览器空闲时处理的，所以多个任务之间会被中断。
@@ -166,10 +180,6 @@ function performUnitOfWork(fiber) {
   // if (fiber.parent) {
   //   fiber.parent.dom.appendChild(fiber.dom)
   // }
-
-  // 创建该节点下的子节点的fiber
-  const elements = fiber.props.children
-  reconcileChildren(fiber, elements)
 
   // 返回下一个任务单元
   if (fiber.child) {
@@ -184,6 +194,56 @@ function performUnitOfWork(fiber) {
     }
     nextFiber = nextFiber.parent
   }
+}
+
+let wipFiber = null
+let hookIndex = null
+
+function updateFunctionComponent(fiber) {
+  wipFiber = fiber
+  hookIndex = 0
+  wipFiber.hooks = []
+  // 通过执行组件函数来获取children
+  const children = [fiber.type(fiber.props)]
+  // 创建该节点下的子节点的fiber
+  reconcileChildren(fiber, children)
+}
+
+function useState(initial) {
+  const oldHook = wipFiber.alternate && wipFiber.alternate.hooks && wipFiber.alternate.hooks[hookIndex]
+  const hook = {
+    state: oldHook ? oldHook.state : initial,
+    queue: []
+  }
+
+  const actions = oldHook ? oldHook.queue : []
+  actions.forEach(action => {
+    hook.state = action(hook.state)
+  })
+
+  const setState = action => {
+    hook.queue.push(action)
+    wipRoot = {
+      dom: currentRoot.dom,
+      props: currentRoot.props,
+      alternate: currentRoot
+    }
+    
+    deletions = []
+    nextUnitOfWork = wipRoot
+  }
+
+  wipFiber.hooks.push(hook)
+  hookIndex++
+  return [hook.state, setState]
+}
+
+function updateHostComponent(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+  // 创建该节点下的子节点的fiber
+  reconcileChildren(fiber, fiber.props.children)
 }
 
 function reconcileChildren(wipFiber, elements) {
@@ -244,5 +304,6 @@ function reconcileChildren(wipFiber, elements) {
 
 const Didact = {
   createElement,
-  render
+  render,
+  useState
 }
